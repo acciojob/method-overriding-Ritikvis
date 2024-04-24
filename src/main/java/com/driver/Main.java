fe5709cb75f9c77cbdca3e7a9c6e1547147dee3f
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-04-24 11:24:42 +0530

    fourth commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // Task 3
         B objB = new B();
-        System.out.println(objB.meth()); // Output will be "Method is overridden in Extended class B"
+        System.out.println(objB.meth());
+    }
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
     }
 }
